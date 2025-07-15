72b371ec253e329fd50c194d5bfde6a4a597ae5d
randymcmillan <randymcmillan@protonmail.com>
2025-07-15 17:34:52 -0400

    src/main.rs:blob_hash:blob_content

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
         .expect("Failed to create blob");
 
     let blob_content = git_vfs.get_object(&blob_hash).expect("Failed to get blob");
-    println!("blob_content: {}", String::from_utf8_lossy(&blob_content));
+    println!("blob_hash: {blob_hash}");
+    println!("blob_content: \"{}\"", String::from_utf8_lossy(&blob_content));
     println!("blob_sha256: {blob_sha256}");
 
     git_vfs
