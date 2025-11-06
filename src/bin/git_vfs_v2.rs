bbcdc75cc1f49951ec20429853bddba5dcf41f52
randymcmillan <randymcmillan@protonmail.com>
2025-11-06 11:37:21 -0500

    git_vfs_v2:intermediate

--- src/bin/git_vfs_v2.rs
+++ src/bin/git_vfs_v2.rs
@@ -2,7 +2,7 @@
 use std::io;
 use futures::{AsyncReadExt, AsyncWriteExt};
 use libp2p::{
-    kad::{store::MemoryStore, GetProvidersResult, QueryId, RecordKey},
+    kad::{store::MemoryStore, QueryId, RecordKey},
     mdns,
     request_response::{self},
     swarm::{NetworkBehaviour},
@@ -157,8 +157,8 @@ mod tests {
     #[test]
     fn test_behaviour_event_from_kademlia_event() {
         let event = KademliaEvent::OutboundQueryProgressed {
-            id: QueryId::from(0),
-            result: libp2p::kad::QueryResult::GetProviders(libp2p::kad::GetProvidersOk {
+            id: QueryId::from(0u64),
+            result: libp2p::kad::QueryResult::GetProviders(GetProvidersOk {
                 key: RecordKey::new(b"test"),
                 providers: Vec::new(),
             }),
